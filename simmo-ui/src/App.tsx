61f30a2466bc898ddbac6c376b78aadb537747e5
thomas-god <thomas.godlewski@icloud.com>
2023-03-29 21:15:35 +0200

    feat(ui): basic App ui

--- simmo-ui/src/App.tsx
+++ simmo-ui/src/App.tsx
@@ -1,29 +1,65 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import LoanDetailsDisplay, { LoanDetails } from './components/LoanDetailsDisplay';
+import MonetaryInput from './components/MonetaryInput';
+import RateInput from './components/RateInput';
+import { parseLoanDetails } from './utils/parseLoanDetails';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [totalInvestment, setTotalInvestment] = useState(300_000);
+  const [monthlyRevenues, setMonthlyRevenues] = useState(3_000);
+  const [durationInMonths, setDurationInMonths] = useState(300);
+  const [annualRate, setAnnualRate] = useState(2.9);
+  const [projectResults, setProjectResults] = useState<LoanDetails | undefined>(undefined);
+  const currencySymbol = '€';
+
+  const onClick = async () => {
+    const url = new URL('http://127.0.0.1:8080/project_analysis');
+
+    url.searchParams.append('yearlyLoanRate', annualRate.toString());
+    url.searchParams.append('loanDuration', durationInMonths.toString());
+    url.searchParams.append('loanAmount', totalInvestment.toString());
+    url.searchParams.append('netMonthlyIncome', monthlyRevenues.toString());
+
+    const response = await fetch(url, {
+      method: 'POST',
+    });
+    setProjectResults(parseLoanDetails(await response.json()));
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight mb-2">
+        Simmo
+      </h2>
+      <div className="flex-auto">
+        <MonetaryInput
+          currencySymbol="€"
+          value={monthlyRevenues}
+          onChange={setMonthlyRevenues}
+          inputTitle="Revenus mensuels"
+        />
+        <MonetaryInput
+          currencySymbol={currencySymbol}
+          value={totalInvestment}
+          onChange={setTotalInvestment}
+          inputTitle="Somme à emprunter"
+        />
+        <MonetaryInput
+          currencySymbol=""
+          value={durationInMonths}
+          onChange={setDurationInMonths}
+          inputTitle="Durée de remboursement (en mois)"
+        />
+        <RateInput rate={annualRate} onChange={setAnnualRate} inputTitle="Taux annuel" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p className="text-lg font medium">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button
+        className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        onClick={onClick}
+      >
+        Calculer votre projet
+      </button>
+      <LoanDetailsDisplay loanDetails={projectResults} />
     </div>
   );
 }

cadf4937faf0e11750817279027f9b4565a7bd81
Thomas Godlewski <48624600+thomas-god@users.noreply.github.com>
2023-03-30 08:56:41 +0200

    Feature/init react/ts with vite & rome (#1)
    * feat(ui): vite create react app
    
    * feat(ui): .nvmrc
    
    * feat(ui): add rome
    
    * feat(ui): add vitest
    
    * feat(ui): add taillwind

---
+++ simmo-ui/vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/', 'src/setupTests.ts'],
+    },
+    clearMocks: true,
+    useAtomics: true,
+  },
+});
